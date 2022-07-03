ed0c1865f6448c49190a9291b197d17cfb7ef6a9
Sangjin Lee <sjlee@kodebox.io>
2022-07-04 00:51:12 +0900

    Modify test

--- myapp/src/Counter.test.js
+++ myapp/src/Counter.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { Counter } from "./Counter";
 
-test('counter test', () => {
-    render(<Counter />);
-    const plus = screen.getByText("+");
-    expect(plus).toBeInTheDocument();
+describe('Counter Test', () => {
+    it('matches snapshot', () => {
+        render(<Counter />);
+        screen.getByText('0');
+    });
+
+    it('shows the props correctly', () => {
+        render(<Counter />);
+        const number = screen.getByText('0');
+        const plus = screen.getByText("+");
+        const minus = screen.getByText("-");
+
+        fireEvent.click(plus);
+        expect(number).toHaveTextContent('1');
+
+        fireEvent.click(minus);
+        fireEvent.click(minus);
+        fireEvent.click(minus);
+        expect(number).toHaveTextContent('-2');
+    });
 });
